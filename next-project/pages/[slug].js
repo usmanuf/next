9e031770ad137c610b9987563f7e0a1a6f34ecac
nayabgull <nayabGull2010@gmail.com>
2021-12-30 12:46:40 +0500

    detail page changes

--- next-project/pages/[slug].js
+++ next-project/pages/[slug].js
@@ -8,6 +8,8 @@ import Image from "next/image";
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "../styles/SingleMakesPage.module.css";
 import moment from "moment";
+import Head from "next/head";
+import Html from "./Html";
 // import '../components/Blog/blog.module.css';
 export async function getServerSideProps(context) {
   let slug = context.query;
@@ -19,9 +21,10 @@ export async function getServerSideProps(context) {
 }
 
 const Testing = (props) => {
+  const articleDetailVariable = {};
   useEffect(() => {
     getRelatedArticles();
-    getArticleDetail(); 
+    getArticleDetail();
   }, []);
 
   let slug = props.slug.slug;
@@ -33,6 +36,7 @@ const Testing = (props) => {
   const [data, setData] = useState([]);
   const [maker, setMaker] = useState("");
   const [articleDetail, setArticleDetail] = useState();
+  const [articleDetail2, setArticleDetail2] = useState();
   const [articles, setArticles] = useState([]);
 
   const getRelatedArticles = () => {
@@ -48,13 +52,15 @@ const Testing = (props) => {
       });
   };
 
-  const getArticleDetail = () => {
+  const getArticleDetailSamePage = (slug) => {
     const slug2 = props.slug.slug.split("crazy-blog-");
     axios
-      .get(URL_API + `article/${slug2[1]}`)
+      .get(URL_API + `article/${slug}`)
       .then((response) => {
         if (response.data.status == 200) {
           setArticleDetail(response.data.article);
+          setArticleDetail2(response.data.article);
+          articleDetailVariable = response.data.article;
         }
       })
       .catch((error) => {
@@ -62,91 +68,53 @@ const Testing = (props) => {
       });
   };
 
-  //   const callUseEffect = () => {
-  //     setInterval(() => {
-  //       getArticleDetail();
-  //     }, [2000]);
-  //   };
+  const getArticleDetail = () => {
+    const slug2 = props.slug.slug.split("crazy-blog-");
+    axios
+      .get(URL_API + `article/${slug2[1]}`)
+      .then((response) => {
+        if (response.data.status == 200) {
+          setArticleDetail(response.data.article);
+          setArticleDetail2(response.data.article);
+          articleDetailVariable = response.data.article;
+        }
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
 
   const location = useRouter();
   var name = props.location?.detail;
-
   return (
     <>
+      <Head>
+        <title>{articleDetail?.title} – Crazy Car Corner</title>
+        <meta
+          name="viewport"
+          content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta
+          name="og:description"
+          content="Are you trying to sell your car that has been damaged? Crazy Car Corner was founded with the goal of making the automobile selling process feasible for you."
+        />
+        <meta
+          name="og:keywords"
+          content="Sell my car, sell any car, who can buy my car, best car scrapping service, cash for my scrap car, used cars buyer, used Cars for Sell, Car Buyers, Used Damaged Cars, online car valuation"
+        />
+        <meta content="#000000" data-react-helmet="true" />
+      </Head>
+
       <div className={""}>
         <div className={styles.HeroiImgBlog}>
           <div className={styles.paddingTopBottomBlog}></div>
         </div>
         <section className="py-5">
-          <div className="container px-4 px-lg-5 my-5">
-            <div className="row gx-4 gx-lg-5 ">
-              <div className="col-md-6">
-                <Image
-                  className="card-img-top mb-5 mb-md-0"
-                  src={`https://lync1869.rideshide.com/` + articleDetail?.image}
-                  alt="..."
-                  height={500}
-                  width={500}
-                />
-                {/* <img
-              className="card-img-top mb-5 mb-md-0"
-              src={AboutImages}
-              alt="..."
-              width="200px"
-              height="500px"
-              style={{ paddingTop: "20px" }}
-            /> */}
-              </div>
-              <div className="col-md-6">
-                <div className="small mb-1">&nbsp; &nbsp;</div>
-                <h2 className="display-6 fw-bolder">{articleDetail?.title}</h2>
-                <div className="fs-5 mb-5">
-                  <p className={`card-title ${styles.cardHeading}`}>Tags</p>
-                  <div
-                    style={{
-                      padding: "10px",
-                    }}
-                  >
-                    {articleDetail?.tag.map((obj) => {
-                      return (
-                        <>
-                          <span
-                            style={{
-                              color: "white",
-                              backgroundColor: "#a9d5a9",
-                              padding: "10px",
-                              borderRadius: "15px",
-                            }}
-                          >
-                            {obj.name}
-                          </span>
-                          &nbsp;
-                        </>
-                      );
-                    })}
-                  </div>
-                  <span></span>
-                </div>
-
-                <div className="fs-5 mb-5">
-                  <p className={`card-title ${styles.cardHeading}`}>
-                    Description
-                  </p>
-                  <div
-                    style={{
-                      padding: "10px",
-                    }}
-                  >
-                    <p className="lead" style={{ fontSize: "15px" }}>
-                      {articleDetail?.article_text}
-
-                      <br></br>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Html
+            articleDetail={articleDetail2}
+            articles={articles}
+            styles={styles}
+          />
         </section>
 
         <div className={"container mb-5"}>
@@ -167,7 +135,9 @@ const Testing = (props) => {
                         as={`/crazy-blog-${article.slug}`}
                         passHref
                       >
-                        <a>
+                        <a
+                          onClick={() => getArticleDetailSamePage(article.slug)}
+                        >
                           <Image
                             height={250}
                             width={350}
@@ -201,268 +171,6 @@ const Testing = (props) => {
               </>
             );
           })}
-        </div>
-      </div>
-
-      {/* <div>
-    <Image
-      className={styles.HerosectionAllMakeSingle}
-      src={ImageURL + maker.hero_image}
-      width={1200}
-      height={600}
-      layout="responsive"
-      alt="sell-make"
-    />
-  </div>
-
-  <div className="container">
-    <div className="row mt-2 p-4">
-      <div className={"m-auto text-center"}>
-        <div className={styles.modelHeading}>
-          <h1>
-            {" "}
-            <span className={styles.spanStyles}>Sell My</span>{" "}
-            {maker.vehicle_name}
-          </h1>
-        </div>
-      </div>
-      <div className={styles.paraWhy}>{maker.description}</div>
-      <div className="col-md-12 col-sm-12">
-        <div className={styles.carModels}>
-          <p>For your instant and FREE valuation,the quick and easy way!</p>
-          <div className={"col-md-4 col-sm-12"}>
-            <div className={styles.btnDiv}>
-              <a
-                href="https://api.whatsapp.com/send?phone=+971558829990&text=I%27m%20interested%20to%20sell%20my%20Car"
-                className={styles.btnHomesSellCar}
-              >
-                <IoLogoWhatsapp className={styles.iconHome} />
-                WhatsApp
-              </a>
-              <Link href="/free-online-valuation">
-                <a className={styles.btnHomesSellCar}>
-                  Get Free Inspection
-                </a>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-      <br />
-      <br />
-      <div className="col-md-12">
-        <div className={styles.h2Div}>
-          <h2 className={styles.h2}>{name} MODELS WE ARE LOOKING TO BUY</h2>
-        </div>
-      </div>
-    </div>
-    <br />
-
-    <div className="row">
-      {data?.map((data, index) => {
-        return (
-          <>
-            {data.image ? (
-              <div key={index} className="col-md-3 col-sm-12">
-                <div className={styles.picDiv}>
-                  <div className={styles.boxStylesModelSingle}>
-                    <Image
-                      className={styles.imageSinglePage}
-                      src={ImageURL + data.image}
-                      width={300}
-                      height={200}
-                      alt="maker"
-                    />
-                  </div>
-                  <div className={styles.boxStylesModelSingleText}>
-                    <p className={styles.pTag}>{data.modle_name}</p>
-                  </div>
-                </div>
-              </div>
-            ) : null}
-          </>
-        );
-      })}
-    </div>
-
-    <br />
-    <br />
-  </div>
-  <div className="col-md-3 col-sm-12"></div>
-  <div>
-    <SellAnyCar />
-  </div> */}
-
-      <div className="container blogContainer">
-        {/* <div className="blogHeroiImg">
-      <p className="blogHeroiImgText">
-        Sell Your Used Car the Easy Way |<br />
-        Get an Instant Cash Offer
-      </p>
-    </div> */}
-        <br />
-        <br />
-        <div className="row rowBlogCard">
-          <div className="col-12 col-md-8 col-lg-9 caroselImageP">
-            <div className="card mb-5">
-              {/* <p className="sliderImageText">19 Oct</p> */}
-              {/* 
-          <Slider {...settings} className="card-img-top">
-            {AboutImages.slice(0, 8).map((data, index) => {
-              return (
-                <div className="mainDiv">
-                  <div key={index} className={"citiesAbout"}>
-                    <div className={"AboutImageCarousel"}>
-                      <div className="pp">
-                        <img className={"imgcity"} src={data.img} alt={"/"} />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-          </Slider> */}
-
-              <br />
-              {/* <div class="card-body singlePageBodyPadding">
-            <span class="card-title titleCardSize ms-4">
-              GM finds its balance with sales of pickups
-            </span>
-            <p class="card-title titleCardSize2 ms-4">
-              By: <span className="spanText">Admin </span> Comment:{" "}
-              <span className="spanText">3</span>
-            </p>
-            <br />
-            <p class="card-text ms-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              aspernatur illum vel sunt libero voluptatum repudiandae valium
-              maxime tenetur.
-            </p>
-            <p class="card-text ms-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              aspernatur illum vel sunt libero voluptatum repudiandae valium
-              maxime tenetur.
-            </p>
-            <div class=" textBackground">
-              <i class="fas fa-quote-left leftQuote"></i>
-              <p class="card-text textStyle">
-                <q>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                  aspernatur illum vel sunt libero voluptatum repudiandae
-                  valium maxime tenetur.
-                </q>
-              </p>
-            </div>
-            <br />
-            <p class="card-text ms-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              aspernatur illum vel sunt libero voluptatum repudiandae valium
-              maxime tenetur.
-            </p>
-            <p class="card-text ms-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              aspernatur illum vel sunt libero voluptatum repudiandae valium
-              maxime tenetur.
-            </p>
-            <div className="imgPContainerDiv">
-              <p class="card-text ms-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                aspernatur illum vel sunt libero voluptatum repudiandae
-                valium maxime tenetur.
-              </p>
-              <img
-                src="./CarBrands/car1.jpg"
-                alt=""
-                style={{ width: "45%", height: "auto" }}
-              />
-            </div>
-            <p class="card-text ms-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              aspernatur illum vel sunt libero voluptatum repudiandae valium
-              maxime tenetur.
-            </p>
-            <br />
-            <br />
-            <div className="shareDiv">
-              <p class="card-text ms-4">
-                TAGS <span className="spanText5">Auto,</span>{" "}
-                <span className="spanText5">Oil,</span>{" "}
-                <span className="spanText5">News</span>
-              </p>
-              <div className="shareDiv2">
-                <p class="card-text px-5">SHARE THIS</p>
-                <a href="">
-                  <i class="fab fa-facebook"></i>
-                </a>{" "}
-                <a href="">
-                  <i class="fab fa-twitter"></i>
-                </a>{" "}
-                <a href="">
-                  <i class="fab fa-google-plus"></i>
-                </a>
-              </div>
-            </div>
-          </div> */}
-            </div>
-            <br /> {/*comments Section to edit */}
-            {/* <div className="commentMainDiv">
-          <div className="commentConatiner">
-            <img className="commentImg" src="./CarBrands/car1.jpg" alt="" />
-            <p className="commentP">
-              <span className="commentAuthor">Admin</span> says: <br />
-              <span className="commentDate">April 5, 2018 at 1:38pm</span>
-            </p>
-          </div>
-          <div className="commetBody">
-            <p className="commetBodyP">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              aspernatur illum vel sunt libero voluptatum repudiandae valium
-              maxime tenetur.
-            </p>
-            <button className="btn btn-light btnComment">Reply</button>
-          </div>
-        </div> */}
-            <br />
-            <br />
-            {/* <p className="replyHeading">Leave a Reply</p>
-        <p className="replyPText">
-          Your email address will not be published. Required fields are
-          marked *
-        </p>
-        <br />
-        <form class="form-block">
-          <div class="form-group">
-            <textarea
-              class="form-input replyTextArea"
-              required=""
-              placeholder="Comment"
-            ></textarea>
-          </div>
-          <div className="form-group">
-            <div class="form-check col form-check-inline">
-              <input
-                className="form-check-input "
-                type="radio"
-                name="inlineRadioOptions"
-                id="inlineRadio3"
-                value="option3"
-              />
-              <label
-                className="form-check-label text-muted"
-                for="inlineRadio3"
-              >
-                Save my name, email, and website in this browser for the
-                next time I comment.
-              </label>
-            </div>
-          </div>
-          <br />
-          <a class="btn btn-primary pull-left replyFormBtn">Post Comment</a>
-        </form> */}
-          </div>
-          <div className="col-12 col-md-4 col-lg-3 smallScreenPadding">
-            {/* <SideBarBlog /> */}
-          </div>
         </div>
       </div>
     </>
